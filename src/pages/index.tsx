6541bb4fb095ec1b61a1d06fae7adc79068902d7
Stanley Filipe <stanley92filipe@gmail.com>
2022-06-12 00:14:05 -0300

    feat: create a banner component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { Flex } from "@chakra-ui/react";
 
 import { Header } from "../components/header";
+import { Banner } from "../components/banner";
 
 export default function Home() {
   return (
     <Flex direction='column'>
       <Header />
+      <Banner />
     </Flex>
   )
 }
