96690b010aa8c38bb7e6993125ad3da47e43065a
Stanley Filipe <stanley92filipe@gmail.com>
2022-06-14 00:18:51 -0300

    feat: create Slider component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,30 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Heading } from "@chakra-ui/react";
 
-import { Header } from "../components/header";
 import { Banner } from "../components/banner";
 import { Categories } from "../components/categories";
+import { Separator } from "../components/separator";
+import Slider from "../components/slider";
 
 export default function Home() {
   return (
     <Flex direction='column'>
+      
       <Banner />
       <Categories />
+      <Separator />
+
+      <Heading
+        textAlign='center'
+        fontWeight='500'
+        mb={['5', '14']}
+        fontSize={['lg', '3xl', '4xl']}
+      >
+        Vamos nessa? <br />
+        Então escolha seu continente
+      </Heading>
+
+      <Slider />
+      
     </Flex>
   )
 }
