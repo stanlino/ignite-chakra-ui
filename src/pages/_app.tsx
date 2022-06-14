3a832e352f2ffb52e3de33c647793e33662f018c
Stanley Filipe <stanley92filipe@gmail.com>
2022-06-13 23:23:06 -0300

    feat: install Swiper component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react"
 import { AppProps } from "next/app"
 
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import { theme } from "../styles/theme"
 
 import { Header } from "../components/header"
