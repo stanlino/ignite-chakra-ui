d13ea147f57adf47ac2ee50942aba5738fe0b32a
Stanley Filipe <stanley92filipe@gmail.com>
2022-06-15 02:58:38 -0300

    feat: create Content continent page component

--- src/pages/continent/[name].tsx
+++ src/pages/continent/[name].tsx
@@ -1,10 +1,16 @@
 import { Flex } from "@chakra-ui/react";
 import { ContinentBanner } from "../../components/c_banner";
+import { Content } from "../../components/c_content";
 
 export default function Continent() {
   return (
     <Flex direction='column'>
       <ContinentBanner />
+
+      <Flex direction='column' maxW='1160px' mx='auto' mb='10' px='1rem'>
+        <Content />
+      </Flex>
+
     </Flex>
   )
 }
